a12e2476f569dbe35d6ded54a72ef7efaa27ae60
vedant <vedantw32@gmail.com>
2025-04-15 16:44:08 +0530

    updated the delte button in checkout page which delted rhe producsts from cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -18,3 +18,5 @@ export function checkMatchingQuantity(matching, itemId,) {
   }
   
 }
+
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,15 @@
-import { cart as cartt} from '/scripts/cart.js'
+import { cart as cartt } from '/scripts/cart.js'
 import { products } from '/scripts/products.js'
 console.log('heloo')
+
 let cart = JSON.parse(localStorage.getItem('cart'))
 
+ui()
+
+function ui() {
+  document.querySelector('.layout-1').innerHTML = '';
 cart.forEach((cartItem) => {
+  
   const itemId = cartItem.itemId;
   
   let matchingItem = '';
@@ -13,9 +19,8 @@ cart.forEach((cartItem) => {
       matchingItem = product
     }
   })
-  
-  
-document.querySelector('.layout-1').innerHTML +=  `
+
+  document.querySelector('.layout-1').innerHTML += `
 <nav class="product-box">
  <div class="product-info">
    <nav class="headline">
@@ -34,7 +39,14 @@ document.querySelector('.layout-1').innerHTML +=  `
      </span>
      <span class="info-quantity">
        Quantity: ${cartItem.quantity}</span>
+   <div class="links" >     
+<span class="update-link">update</span>
+<span class="delete-link"
+data-item-id ="${cartItem.itemId}"
+>delete</span>
    </div>
+   
+</div>
    
   <div class="delivery-info">
     <span class=info-name>
@@ -76,6 +88,7 @@ document.querySelector('.layout-1').innerHTML +=  `
    class="delivery-input"
    name="${cartItem.itemId}">
    
+   
    <section class="part--2">
    <div class="delivery-date">
      Tuesday, June 21
@@ -93,9 +106,39 @@ document.querySelector('.layout-1').innerHTML +=  `
   </nav>
   `;
   
+  
 });
+}
 
 document.querySelector('.clear')
-.addEventListener('click', ()=>{
-  localStorage.removeItem('cart')
+  .addEventListener('click', () => {
+    localStorage.removeItem('cart')
+  })
+
+
+deletItems();
+function deletItems(){
+  let btnId;
+  let updated = [];
+
+document.querySelectorAll('.delete-link').forEach((link) => {
+
+  link.addEventListener('click', (event) => {
+    btnId = event.target.dataset.itemId;
+    updated = [];
+
+    cart.forEach( (cartItem)=>{
+    if (btnId !== cartItem.itemId) {
+      updated.push(cartItem)
+      
+    }
+    })
+  
+   cart = updated;
+   localStorage.setItem('cart', JSON.stringify(cart))
+   ui()
+   deletItems()
+  })
+
 })
+}
